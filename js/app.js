4b66b0e14750c3d4dff07798d5a1620ed6905028
Hrummer <vadymhomyak@gmail.com>
2015-10-02 01:48:54 +0300

    Created new layout

--- js/app.js
+++ js/app.js
@@ -27,43 +27,81 @@ function renderProductsUI() {
         });
     });
 
-    lis.find(".editBtn").on("click", function () {
+    lis.find('.editBtn').on('click', function () {
         var li = $(this).parent();
-        li.find(".editBtn").hide();
-        li.find(".editInput").show();
-        li.find(".cancelBtn").show();
-        li.find(".okayBtn").show();
+        li.find('.editBtn').hide();
+        li.find('.editInput').show();
+        li.find('.cancelBtn').show();
+        li.find('.okayBtn').show();
     });
 
-    lis.find(".cancelBtn").on("click", function () {
+    lis.find('.cancelBtn').on('click', function () {
         var li = $(this).parent();
-        var input = li.find(".editInput");
-        input.val("");
+        var input = li.find('.editInput');
+        input.val('');
         input.hide();
-        li.find(".cancelBtn").hide();
-        li.find(".okayBtn").hide();
-        li.find(".editBtn").show();
+        li.find('.cancelBtn').hide();
+        li.find('.okayBtn').hide();
+        li.find('.editBtn').show();
     });
 
-    lis.find(".okayBtn").on("click", function () {
+    lis.find('.editInput').on('input', function () {
+        var input = $(this);
+        var li = $(this).parent();
+        
+        if (input.val().length > 30) {
+            li.find('.warn').show()
+        }
+        else li.find('.warn').hide()        
+    });
+    
+    lis.find('.editInput').on('keyup', function (e) {
+        if (e.keyCode === 13){
+            var li = $(this).parent();
+            var input = $(this);
+            if (input.val().length > 30) return;
+            else {
+                var product = products[li.attr('data-index')];
+                $.post('http://localhost:3000/products/'+product.id+'/edit', { editedProduct: input.val() }, function () {
+                    product.name = input.val();
+                    renderProductsUI();
+                });
+            }
+        }
+    });
+
+    lis.find('.okayBtn').on('click', function () {
         var li = $(this).parent();
         var product = products[li.attr('data-index')];
-        var input = li.find(".editInput");
-        $.post('http://localhost:3000/products/'+product.id+'/edit', { editedProduct: input.val() }, function () {
+        var input = li.find('.editInput');
+        if (input.val().length > 30) return;
+        else{
+            $.post('http://localhost:3000/products/'+product.id+'/edit', { editedProduct: input.val() }, function () {
                 product.name = input.val();
                 renderProductsUI();
-        });
+            });
+        }
     });
 
     var productsInput = $('.productsInput');
 
     function addProduct () {
         var product = productsInput.val();
-        $.post('http://localhost:3000/products/add', { newProduct: product }, function (data) {
-            products.push({id:data, name:product});
-            renderProductsUI();
-        });
+        if (product.length > 30) return;
+        else{
+            $.post('http://localhost:3000/products/add', { newProduct: product }, function (data) {
+                products.push({id:data, name:product});
+                renderProductsUI();
+            });
+        }
     }
+
+    productsInput.on('input', function () {
+        if (productsInput.val().length > 30) {
+            $('.addingWarn').show()
+        }
+        else $('.addingWarn').hide()
+    });
 
     productsInput.on('keyup', function (e) {
         if (e.keyCode === 13) addProduct();
@@ -79,10 +117,10 @@ function search (arrayOfObjects, value) {
 }
 
 function searchAndRender () {
-        var cachedProducts = products;
-        products = search(products, searchInput.val());
-        renderProductsUI();
-        products = cachedProducts;
+    var cachedProducts = products;
+    products = search(products, searchInput.val());
+    renderProductsUI();
+    products = cachedProducts;
 }
 
 searchInput.on('keyup', function (e) {
